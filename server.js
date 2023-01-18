8dda69f5e52073df39add05a938a6a09bea02ddb
guntak <imbmi4@naver.com>
2023-01-18 23:42:07 +0900

    세션 없는거

--- server.js
+++ server.js
@@ -76,3 +76,40 @@ app.post('/edit/:id', function(req, res) {
         res.redirect('/list');
     })
 })
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const session = require('express-session');
+
+app.use(session({secret : 'guntak', resave : true, saveUninitialized : false}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', function(req, res){
+    res.render('login.ejs')
+});
+
+app.post('/login', passport.authenticate('local',{
+    failureRedirect : '/fail'
+}), function(req, res){
+    res.redirect('/')
+});
+
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+  }, function (입력한아이디, 입력한비번, done) {
+    console.log(입력한아이디, 입력한비번);
+    db.collection('users').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+      if (에러) return done(에러)
+  
+      if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+      if (입력한비번 == 결과.password) {
+        return done(null, 결과)
+      } else {
+        return done(null, false, { message: '비번틀렸어요' })
+      }
+    })
+  }));
